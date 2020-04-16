c1d407a0a7341b142530539d5651c5181514575a
rahmithamn <rahmithasaid@gmail.com>
2020-04-17 03:40:53 +0800

    add RecyclerView for Search Fragment

--- app/src/main/java/com/rmn/githubapi/SearchFragment.kt
+++ app/src/main/java/com/rmn/githubapi/SearchFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_detail_user.*
+import kotlinx.android.synthetic.main.fragment_search.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class SearchFragment : Fragment() {
-
+    private val list = ArrayList<User>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,5 +21,37 @@ class SearchFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
+    fun getListUser(): ArrayList<User> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataUsername = resources.getStringArray(R.array.data_username)
+        val dataFollower = resources.getIntArray(R.array.data_follower)
+        val dataFollowing = resources.getIntArray(R.array.data_following)
+        val dataPhoto = resources.getStringArray(R.array.data_photo)
+        val listUser = ArrayList<User>()
+        for (position in dataName.indices) {
+            val user = User(
+                dataName[position],
+                dataUsername[position],
+                dataFollower[position],
+                dataFollowing[position],
+                dataPhoto[position]
+            )
+            listUser.add(user)
+        }
+        return listUser
+    }
 
+    private fun showRecyclerList() {
+        rv_user.layoutManager = LinearLayoutManager(this.requireContext())
+        val listHeroAdapter = UserAdapter(list)
+        rv_user.adapter = listHeroAdapter
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        rv_user.setHasFixedSize(true)
+        list.addAll(getListUser())
+        showRecyclerList()
+    }
 }
